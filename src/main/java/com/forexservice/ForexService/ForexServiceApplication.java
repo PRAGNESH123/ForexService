56592cc9ea26a1f0a961feb15150d7bf534499fb
Poojary <pragnesh-ramachandra.poojary@capgemini.com>
2023-11-22 20:19:55 +0530

    Adding Backend Project

---
+++ src/main/java/com/forexservice/ForexService/ForexServiceApplication.java
@@ -0,0 +1,15 @@
+package com.forexservice.ForexService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+public class ForexServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForexServiceApplication.class, args);
+	}
+
+}
